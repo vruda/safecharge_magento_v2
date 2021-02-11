f7224192022e8f086f6859de75794c59519d3956
miroslav-sc <miroslavs@safecharge.com>
2021-02-11 10:44:59 +0200

    small changes

--- Model/Config.php
+++ Model/Config.php
@@ -174,9 +174,9 @@ class Config
             return;
         }
         
-        $logsPath    = $this->directory->getPath('log');
-        $d            = $data;
-        $string        = '';
+        $logsPath	= $this->directory->getPath('log');
+        $d			= $data;
+        $string		= '';
         
         if (!empty($data)) {
             if (is_array($data)) {
